82678b816207ebed8fd89df8d3158374bba72226
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2026-05-10 01:14:19 -0400

    remove unnecessary cors on backend (for now)

--- backend/src/main/kotlin/Http.kt
+++ backend/src/main/kotlin/Http.kt
@@ -6,13 +6,13 @@ import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 
 fun Application.configureHttp() {
-  install(CORS) {
-    allowMethod(HttpMethod.Options)
-    allowMethod(HttpMethod.Put)
-    allowMethod(HttpMethod.Delete)
-    allowMethod(HttpMethod.Patch)
-    allowHeader(HttpHeaders.Authorization)
-    allowHeader("MyCustomHeader")
-    anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-  }
+  // install(CORS) {
+  //   allowMethod(HttpMethod.Options)
+  //   allowMethod(HttpMethod.Put)
+  //   allowMethod(HttpMethod.Delete)
+  //   allowMethod(HttpMethod.Patch)
+  //   allowHeader(HttpHeaders.Authorization)
+  //   allowHeader("MyCustomHeader")
+  //   anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+  // }
 }
