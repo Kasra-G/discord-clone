9ed475fc67b3837de7320b5fa2178adfc4e9d0d6
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2026-05-10 12:45:22 -0400

    refactor(frontend): move backend comm to chat route

--- frontend/src/routes/chat/+page.server.ts
+++ frontend/src/routes/chat/+page.server.ts
@@ -1,7 +1,6 @@
 import { BACKEND_URL } from '$env/static/private';
-import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load = async () => {
 	const response = await fetch(BACKEND_URL);
 	return {
 		serverMessage: `hello from backend: ${await response.text()}`
