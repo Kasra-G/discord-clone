18824c0a9bf08096e8784a05dcfd259270cad0d2
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2026-05-12 03:53:23 -0400

    feat(websocket): exponential backoff & jitter

--- frontend/src/lib/websocket.svelte.ts
+++ frontend/src/lib/websocket.svelte.ts
@@ -1,15 +1,22 @@
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
+const MAX_DELAY_MS = 10000;
+const INITIAL_DELAY_MS = 100;
 class MessageService {
 	private ws?: WebSocket;
-	public readonly messages = $state<string[]>([]);
-	private _status = $state<'DISCONNECTED' | 'CONNECTING' | 'CONNECTED'>('DISCONNECTED');
-	private reconnectAttempts = 0;
+	private _messages = $state<string[]>([]);
+	private _status = $state<'DISCONNECTED' | 'RECONNECTING' | 'CONNECTING' | 'CONNECTED'>(
+		'DISCONNECTED'
+	);
 	private reconnectId?: ReturnType<typeof setTimeout>;
-	private reconnectDelayMs = 100;
+	private reconnectDelayMs = INITIAL_DELAY_MS;
 
 	public get status() {
 		return this._status;
+	}
+
+	public get messages() {
+		return this._messages;
 	}
 
 	constructor() {}
@@ -24,15 +31,15 @@ class MessageService {
 
 		this.ws.addEventListener('open', () => {
 			this._status = 'CONNECTED';
+			this._messages = [];
 			this.resetReconnectSettings();
 		});
 
 		this.ws.addEventListener('message', (messageEvent) => {
-			this.messages.push(messageEvent.data);
+			this._messages.push(messageEvent.data);
 		});
 
 		this.ws.addEventListener('close', () => {
-			this._status = 'DISCONNECTED';
 			this.handleReconnect();
 		});
 	}
@@ -43,15 +50,14 @@ class MessageService {
 
 	private resetReconnectSettings() {
 		if (this.reconnectId) clearTimeout(this.reconnectId);
-		this.reconnectAttempts = 0;
-		this.reconnectDelayMs = 100;
+		this.reconnectDelayMs = INITIAL_DELAY_MS;
 	}
 
 	private handleReconnect() {
-		this.reconnectAttempts++;
-		this.reconnectDelayMs *= 2;
+		this.reconnectDelayMs = Math.min(this.reconnectDelayMs * 2, MAX_DELAY_MS) * Math.random();
 		this.reconnectId = setTimeout(() => {
 			this.connect();
+			this._status = 'RECONNECTING';
 		}, this.reconnectDelayMs);
 	}
 }
