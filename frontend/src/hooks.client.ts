63d9fcd129569960ed3d5b5f1a822413c774f56a
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2026-07-12 01:42:44 -0700

    feat(otel): disable client telemetry

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,30 +1,30 @@
-import { SimpleSpanProcessor, WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
-import { resourceFromAttributes } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+// import { SimpleSpanProcessor, WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+// import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+// import { ZoneContextManager } from '@opentelemetry/context-zone';
+// import { registerInstrumentations } from '@opentelemetry/instrumentation';
+// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+// import { resourceFromAttributes } from '@opentelemetry/resources';
+// import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 
 export async function init() {
-	const provider = new WebTracerProvider({
-		resource: resourceFromAttributes({
-			[ATTR_SERVICE_NAME]: 'sveltekit-frontend',
-		}),
-		spanProcessors: [new SimpleSpanProcessor(new OTLPTraceExporter())],
-	});
-
-	provider.register({
-		contextManager: new ZoneContextManager(),
-	});
-
-	registerInstrumentations({
-		instrumentations: [
-			getWebAutoInstrumentations({
-				'@opentelemetry/instrumentation-fetch': {
-					propagateTraceHeaderCorsUrls: [/localhost:\d+/],
-				},
-			}),
-		],
-	});
+	// const provider = new WebTracerProvider({
+	// 	resource: resourceFromAttributes({
+	// 		[ATTR_SERVICE_NAME]: 'sveltekit-frontend',
+	// 	}),
+	// 	spanProcessors: [new SimpleSpanProcessor(new OTLPTraceExporter())],
+	// });
+	//
+	// provider.register({
+	// 	contextManager: new ZoneContextManager(),
+	// });
+	//
+	// registerInstrumentations({
+	// 	instrumentations: [
+	// 		getWebAutoInstrumentations({
+	// 			'@opentelemetry/instrumentation-fetch': {
+	// 				propagateTraceHeaderCorsUrls: [/localhost:\d+/],
+	// 			},
+	// 		}),
+	// 	],
+	// });
 }
