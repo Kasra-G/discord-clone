419b6a59a600f752ef3f6eafacf538adc480a82c
Kasra Ghaffari <18647702+Kasra-G@users.noreply.github.com>
2026-07-18 03:04:13 -0700

    fix(backend): remove logging for healthcheck endpoint

--- backend/src/main/kotlin/CallLogging.kt
+++ backend/src/main/kotlin/CallLogging.kt
@@ -3,6 +3,7 @@ package com.ghkasra.discordclone
 import io.ktor.server.application.*
 import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.request.path
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -10,7 +11,11 @@ import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 
 fun Application.configureCallLogging() {
-  install(CallLogging)
+  install(CallLogging) {
+    filter { call ->
+      call.request.path() != "/api/health"
+    }
+  }
   val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
   install(MicrometerMetrics) {
     registry = appMicrometerRegistry
